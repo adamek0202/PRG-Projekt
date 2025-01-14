0dd5fba2be9db140cfacad743a36f21f20f13f2a
Adam Benda <adamek0584@gmail.com>
2025-01-14 15:23:05 +0100

    Tlačítka

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,12 +33,18 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.Produkt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Cena = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Mnozstvi = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.sumBox = new System.Windows.Forms.TextBox();
+            this.sumLabel = new System.Windows.Forms.Label();
+            this.downButton = new System.Windows.Forms.Button();
+            this.upButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -46,7 +52,8 @@
             this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.Produkt,
-            this.Cena});
+            this.Cena,
+            this.Mnozstvi});
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
             listViewGroup1.Header = "Menu 1";
@@ -59,12 +66,12 @@
             this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.listView1.HideSelection = false;
             this.listView1.LabelWrap = false;
-            this.listView1.Location = new System.Drawing.Point(9, 13);
+            this.listView1.Location = new System.Drawing.Point(13, 13);
             this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
             this.listView1.ShowGroups = false;
-            this.listView1.Size = new System.Drawing.Size(306, 711);
+            this.listView1.Size = new System.Drawing.Size(331, 717);
             this.listView1.TabIndex = 0;
             this.listView1.TileSize = new System.Drawing.Size(332, 40);
             this.listView1.UseCompatibleStateImageBehavior = false;
@@ -72,21 +79,31 @@
             // 
             // Produkt
             // 
+            this.Produkt.DisplayIndex = 1;
             this.Produkt.Text = "Produkt";
             this.Produkt.Width = 219;
             // 
             // Cena
             // 
+            this.Cena.DisplayIndex = 2;
+            this.Cena.Tag = "Cena";
             this.Cena.Text = "Cena";
             this.Cena.Width = 85;
+            // 
+            // Mnozstvi
+            // 
+            this.Mnozstvi.DisplayIndex = 0;
+            this.Mnozstvi.Text = "";
+            this.Mnozstvi.Width = 27;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(322, 13);
+            this.button1.Location = new System.Drawing.Point(351, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 85);
+            this.button1.Size = new System.Drawing.Size(96, 85);
             this.button1.TabIndex = 1;
             this.button1.Text = "Zinger";
             this.button1.UseVisualStyleBackColor = false;
@@ -95,66 +112,144 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(421, 13);
+            this.button2.Location = new System.Drawing.Point(453, 13);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 85);
+            this.button2.Size = new System.Drawing.Size(96, 85);
             this.button2.TabIndex = 1;
             this.button2.Text = "Zinger\r\nDouble";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // RemoveButton
             // 
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(322, 702);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(93, 59);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Smazat";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.RemoveButton.BackColor = System.Drawing.Color.Red;
+            this.RemoveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RemoveButton.ForeColor = System.Drawing.Color.White;
+            this.RemoveButton.Location = new System.Drawing.Point(351, 698);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(93, 59);
+            this.RemoveButton.TabIndex = 2;
+            this.RemoveButton.Text = "Smazat položku";
+            this.RemoveButton.UseVisualStyleBackColor = false;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(520, 12);
+            this.button4.Location = new System.Drawing.Point(351, 195);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(106, 86);
+            this.button4.Size = new System.Drawing.Size(96, 85);
             this.button4.TabIndex = 1;
             this.button4.Text = "Zinger\r\nDouble\r\nMenu";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label1
+            // sumLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 734);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 23);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Celkem";
+            this.sumLabel.AutoSize = true;
+            this.sumLabel.Location = new System.Drawing.Point(12, 734);
+            this.sumLabel.Name = "sumLabel";
+            this.sumLabel.Size = new System.Drawing.Size(76, 23);
+            this.sumLabel.TabIndex = 3;
+            this.sumLabel.Text = "Celkem";
             // 
-            // sumBox
+            // downButton
             // 
-            this.sumBox.Enabled = false;
-            this.sumBox.Location = new System.Drawing.Point(94, 731);
-            this.sumBox.Name = "sumBox";
-            this.sumBox.Size = new System.Drawing.Size(221, 30);
-            this.sumBox.TabIndex = 4;
+            this.downButton.BackColor = System.Drawing.Color.Black;
+            this.downButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.downButton.ForeColor = System.Drawing.Color.White;
+            this.downButton.Location = new System.Drawing.Point(351, 633);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(93, 59);
+            this.downButton.TabIndex = 2;
+            this.downButton.Text = "Dolu";
+            this.downButton.UseVisualStyleBackColor = false;
+            this.downButton.Click += new System.EventHandler(this.downButton_Click);
+            // 
+            // upButton
+            // 
+            this.upButton.BackColor = System.Drawing.Color.Black;
+            this.upButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.upButton.ForeColor = System.Drawing.Color.White;
+            this.upButton.Location = new System.Drawing.Point(351, 568);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(93, 59);
+            this.upButton.TabIndex = 2;
+            this.upButton.Text = "Nahoru";
+            this.upButton.UseVisualStyleBackColor = false;
+            this.upButton.Click += new System.EventHandler(this.upButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.BackColor = System.Drawing.Color.Red;
+            this.CancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelButton.ForeColor = System.Drawing.Color.White;
+            this.CancelButton.Location = new System.Drawing.Point(450, 698);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(93, 59);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Storno účtu";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.BackColor = System.Drawing.Color.Yellow;
+            this.NoButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.NoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NoButton.ForeColor = System.Drawing.Color.Black;
+            this.NoButton.Location = new System.Drawing.Point(450, 633);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(93, 59);
+            this.NoButton.TabIndex = 2;
+            this.NoButton.Text = "Ne";
+            this.NoButton.UseVisualStyleBackColor = false;
+            this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(351, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 85);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Hranolky malé";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button5.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(453, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 85);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Hranolky velké";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1342, 773);
-            this.Controls.Add(this.sumBox);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1342, 766);
+            this.Controls.Add(this.sumLabel);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.RemoveButton);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
@@ -175,10 +270,16 @@
         private System.Windows.Forms.ColumnHeader Cena;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button RemoveButton;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox sumBox;
+        private System.Windows.Forms.Label sumLabel;
+        private System.Windows.Forms.Button downButton;
+        private System.Windows.Forms.Button upButton;
+        private System.Windows.Forms.ColumnHeader Mnozstvi;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Projekt
@@ -19,27 +15,47 @@ namespace Projekt
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void SelectPreviousItem()
         {
-            var group = new ListViewGroup();
-            AddHeadItem("Zinger double", 130, group);
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                int currentIndex = listView1.SelectedIndices[0];
+                if (currentIndex > 0)
+                {
+                    listView1.Items[currentIndex].Selected = false;
+                    listView1.Items[currentIndex - 1].Selected = true;
+                    listView1.Items[currentIndex - 1].EnsureVisible();
+                }
+            }
+            else if (listView1.Items.Count > 0)
+            {
+                listView1.Items[listView1.Items.Count - 1].Selected = true;
+                listView1.Items[listView1.Items.Count - 1].EnsureVisible();
+            }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void SelectNextItem()
         {
-            if (listView1.SelectedItems.Count > 0 && listView1.SelectedItems[0].Group != null)
+            if (listView1.SelectedIndices.Count > 0)
             {
-                foreach (var item in listView1.SelectedItems[0].Group.Items)
+                int currentIndex = listView1.SelectedIndices[0];
+                if (currentIndex < listView1.Items.Count - 1)
                 {
-                    listView1.Items.Remove((ListViewItem)item);
+                    listView1.Items[currentIndex].Selected = false;
+                    listView1.Items[currentIndex + 1].Selected = true;
+                    listView1.Items[currentIndex + 1].EnsureVisible();
                 }
-                //UpdateSumPrice(-Convert.ToInt32(listView1.SelectedItems[0].Group.Items[1].SubItems[1]));
+            }
+            else if (listView1.Items.Count > 0)
+            {
+                listView1.Items[0].Selected = true;
+                listView1.Items[0].EnsureVisible();
             }
         }
 
         private void AddHeadItem(string name, int price, ListViewGroup group)
         {
-            listView1.Items.Add(new ListViewItem(new string[] { name, price.ToString() + "Kč"}, group) { BackColor = Color.Orange});
+            listView1.Items.Add(new ListViewItem(new string[] { name, price.ToString() + "Kč" , "1"}, group) { BackColor = Color.Orange });
             UpdateSumPrice(price);
         }
 
@@ -47,21 +63,68 @@ namespace Projekt
         {
             foreach (var item in names)
             {
-                listView1.Items.Add(new ListViewItem(item, group) { BackColor = Color.Yellow }); 
+                listView1.Items.Add(new ListViewItem(item, group) { BackColor = Color.Yellow });
             }
         }
 
         private void UpdateSumPrice(int price)
         {
             SumPrice += price;
-            sumBox.Text = SumPrice.ToString() + "Kč";
+            sumLabel.Text = "Celkem " + SumPrice.ToString() + "Kč";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var group = new ListViewGroup();
+            AddHeadItem("Zinger double", 130, group);
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0 && listView1.SelectedItems[0].Group != null)
+            {
+                if (listView1.SelectedItems[0].Group.Items.Count > 0)
+                {
+                    UpdateSumPrice(-Convert.ToInt32(new string(listView1.SelectedItems[0].Group.Items[0].SubItems[1].Text.Where(char.IsDigit).ToArray())));
+
+                }
+                foreach (var item in listView1.SelectedItems[0].Group.Items)
+                {
+                    listView1.Items.Remove((ListViewItem)item);
+                }
+            }
+        }
+        
+        private void downButton_Click(object sender, EventArgs e)
+        {
+            SelectNextItem();
+        }
+        
+        private void upButton_Click(object sender, EventArgs e)
+        {
+            SelectPreviousItem();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             var group = new ListViewGroup();
             AddHeadItem("Zinger double menu", 210, group);
-            AddSubItem(new string[] { "Zinger double", "Velke hranolky", "Bezedny napoj" }, group);
+            AddSubItem(new string[] { "Zinger double", "Male hranolky", "Bezedny napoj" }, group);
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.Groups.Clear();
+            UpdateSumPrice(-SumPrice);
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            if(listView1.SelectedItems.Count == 1)
+            {
+                listView1.SelectedItems[0].BackColor = Color.Red;
+            }
         }
     }
 }
