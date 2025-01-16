399ba813a259ceca1cf7f89735cc3d0c50130697
Adam Benda <adamek0584@gmail.com>
2025-01-16 15:27:43 +0100

    Úklid

--- NativeFunctions.cs
+++ NativeFunctions.cs
@@ -9,8 +9,26 @@ namespace Projekt
 {
     internal class NativeFunctions
     {
+
+        // Konstanty pro specifikaci stylů a scrollbarů
+        public const int GWL_STYLE = -16;
+        public const int WS_VSCROLL = 0x00200000; // Styl pro vertikální scrollbar
+        public const int WS_HSCROLL = 0x00100000; // Styl pro horizontální scrollbar
+        public const int SB_VERT = 1; // Vertikální scrollbar
+
         [DllImport("uxtheme.dll", CharSet = CharSet.Unicode)]
-        private static extern int SetWindowTheme(IntPtr hWnd, string appName, string partList);
+        public static extern int SetWindowTheme(IntPtr hWnd, string appName, string partList);
+
+        // Import WinAPI funkce SetWindowLong a GetWindowLong
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        // Import WinAPI funkce ShowScrollBar
+        [DllImport("user32.dll")]
+        public static extern bool ShowScrollBar(IntPtr hWnd, int wBar, bool bShow);
 
         public static void DisableVisualStyles(Control control)
         {
--- ScrollListView.cs
+++ ScrollListView.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using System;
 
 //Neprovádět bezdůvodné zásahy do logiky, hrozí rozbití aplikace
@@ -8,41 +7,22 @@ namespace Projekt
 {
     public class ListViewWithScrollBar : ListView
     {
-        // Import WinAPI funkce SetWindowLong a GetWindowLong
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        // Import WinAPI funkce ShowScrollBar
-        [DllImport("user32.dll")]
-        private static extern bool ShowScrollBar(IntPtr hWnd, int wBar, bool bShow);
-
-        // Konstanty pro specifikaci stylů a scrollbarů
-        private const int GWL_STYLE = -16;
-        private const int WS_VSCROLL = 0x00200000; // Styl pro vertikální scrollbar
-        private const int WS_HSCROLL = 0x00100000; // Styl pro horizontální scrollbar
-        private const int SB_VERT = 1; // Vertikální scrollbar
-
         protected override void OnHandleCreated(EventArgs e)
         {
             base.OnHandleCreated(e);
 
             // Přidání stylů scrollbarů
-            int style = GetWindowLong(this.Handle, GWL_STYLE);
-            SetWindowLong(this.Handle, GWL_STYLE, style | WS_VSCROLL);
-
-            // Vynucení zobrazení scrollbarů
-            ShowScrollBar(this.Handle, SB_VERT, true);
+            int style = NativeFunctions.GetWindowLong(this.Handle, NativeFunctions.GWL_STYLE);
+            NativeFunctions.SetWindowLong(this.Handle, NativeFunctions.GWL_STYLE, style | NativeFunctions.WS_VSCROLL);
+            // Vynucení zobrazení scrollbar
+            NativeFunctions.ShowScrollBar(this.Handle, NativeFunctions.SB_VERT, true);
         }
 
         protected override void WndProc(ref Message m)
         {
             base.WndProc(ref m);
-
             // Zajisti, že scrollbar zůstane viditelný
-            ShowScrollBar(this.Handle, SB_VERT, true);
+            NativeFunctions.ShowScrollBar(this.Handle, NativeFunctions.SB_VERT, true);
         }
     }
 }
