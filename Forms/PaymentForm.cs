ac5d3551722d4be6baba77ffb80a1abf64edfdfd
Adam Benda <adamek0584@gmail.com>
2025-01-22 13:59:24 +0100

    Tisk účtenek(snad už funkční)

--- Forms/PaymentForm.cs
+++ Forms/PaymentForm.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Drawing.Printing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using static Projekt.BasicTheme;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ESC_POS_USB_NET.Printer;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Projekt.Forms
 {
     public partial class PaymentForm : Form
     {
         private int Price;
+
+        private enum Payments
+        {
+            Cash,
+            CashExact,
+            Card
+        }
 
         private PrintDocument printDocument;
 
@@ -23,10 +31,106 @@ namespace Projekt.Forms
             ReallyCenterToScreen(this);
         }
 
-        private void PrintReceipt()
+        private void ConsolePrint(Payments paymentType)
         {
-            var printer = new UsbEscPosPrinter("lpt1");
-            printer.PrintText("Pokladna");
+            Console.WriteLine("Vítejte");
+            for (int i = 0; i < 32; i++) Console.Write("-");
+            Console.WriteLine();
+            int padding;
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].SubItems.Count > 1)
+                {
+                    Console.Write(listView1.Items[i].Text);
+                    padding = 32 - (listView1.Items[i].Text.Length + listView1.Items[i].SubItems[1].Text.Length);
+                    for (int index = 0; index < padding; index++) Console.Write(" ");
+                    Console.WriteLine(listView1.Items[i].SubItems[1].Text);
+                }
+                else
+                {
+                    Console.Write("-" + listView1.Items[i].Text);
+                    Console.WriteLine();
+                }
+            }
+            for (int i = 0; i < 32; i++) Console.Write("-");
+            Console.WriteLine();
+            padding = 23 - Price.ToString().Length;
+            Console.Write("Celkem");
+            for (int i = 0; i < padding; i++) Console.Write(" ");
+            Console.WriteLine($"{Price} kč");
+            switch (paymentType)
+            {
+                case Payments.Cash | Payments.CashExact:
+                    padding = 20;
+                    Console.Write("Hotově");
+                    for (int i = 0; i < padding; i++) Console.Write(" ");
+                    if (paymentType == Payments.Cash)
+                    {
+                        Console.WriteLine($"{PayedTextBox.Text} Kč");
+                        padding = 20;
+                        Console.Write("Vratit");
+                        for (int i = 0; i < padding; i++) Console.Write(" ");
+                        Console.WriteLine($"{int.Parse(PayedTextBox.Text) - Price} Kč"); 
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Price} Kč");
+                    }
+                    break;
+            }
+            for (int i = 0; i < 32; i++) Console.Write("-");
+            Console.WriteLine();
+        }
+
+        private void PrintReceipt(Payments paymentType)
+        {
+            ConsolePrint(paymentType);
+            Printer printer = new Printer("POS-58-Series");
+            printer.AlignCenter();
+            printer.Append("Pokladna");
+            printer.AlignLeft();
+            printer.Separator();
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].SubItems.Count > 1)
+                {
+                    printer.AppendWithoutLf(listView1.Items[i].Text);
+                    printer.AlignRight();
+                    printer.Append(listView1.Items[i].SubItems[1].Text);
+                    printer.AlignLeft();
+                }
+                else
+                {
+                    printer.Append($"-{listView1.Items[i].Text}");
+                }
+            }
+            printer.Separator();
+            printer.AppendWithoutLf("Celkem");
+            printer.AlignRight();
+            printer.Append($"{Price} Kč");
+            printer.AlignLeft();
+            switch (paymentType)
+            {
+                case Payments.Cash | Payments.CashExact:
+                    printer.AppendWithoutLf("Hotově");
+                    printer.AlignRight();
+                    printer.Append($"{PayedTextBox.Text} Kč");
+                    printer.AlignLeft();
+                    printer.AppendWithoutLf("Vratit");
+                    if (paymentType == Payments.Cash)
+                    {
+                        printer.AlignRight();
+                        printer.Append($"{int.Parse(PayedTextBox.Text) - Price} Kč"); 
+                    }
+                    break;
+                case Payments.Card:
+                    printer.AppendWithoutLf("Karta");
+                    printer.AlignRight();
+                    printer.Append($"{Price} Kč");
+                    break;
+            }
+            printer.AlignLeft();
+            printer.Separator();
         }
 
         protected override void OnHandleCreated(EventArgs e)
@@ -79,7 +183,7 @@ namespace Projekt.Forms
         {
             if(Convert.ToInt32(PayedTextBox.Text) >= Price)
             {
-                PrintReceipt();
+                PrintReceipt(Payments.Cash);
                 var returnBox = new TenderedReturnForm(Convert.ToInt32(PayedTextBox.Text) - Price);
                 returnBox.ShowDialog();
                 DialogResult = DialogResult.OK;
@@ -96,6 +200,7 @@ namespace Projekt.Forms
 
         private void ExactCashButton_Click(object sender, EventArgs e)
         {
+            PrintReceipt(Payments.CashExact);
             DialogResult = DialogResult.OK;
             Close();
         }
